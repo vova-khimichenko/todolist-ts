78a88682f3caee8515cd65773ab9b8fb3a6c3af0
vovkahimik <vovkahimik@gmail.com>
2021-02-13 13:47:46 +0300

    Updates

---
+++ precache-manifest.3a34bcb6dee5945b0df689ff028c50a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db79c4ccde3d6ab1755d1d469819e714",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "adc71ed3681cfff42832",
+    "url": "/todolist-ts/static/css/main.2fb52273.chunk.css"
+  },
+  {
+    "revision": "e147acb09074e2b1d076",
+    "url": "/todolist-ts/static/js/2.721ceb6e.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/todolist-ts/static/js/2.721ceb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc71ed3681cfff42832",
+    "url": "/todolist-ts/static/js/main.95f199d2.chunk.js"
+  },
+  {
+    "revision": "8bdf15c7027486d2fcd1",
+    "url": "/todolist-ts/static/js/runtime-main.ca93a274.js"
+  }
+]);
